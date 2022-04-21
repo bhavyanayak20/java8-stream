568448508a6f1ca9a3596cec0d84a8c4b3e339a8
Bhavya <Bhavya@DESKTOP-TE12S0C>
2022-04-21 10:51:20 +0530

    Basic eclipse code

---
+++ GitHubProjectExample/src/main/java/com/basicprogram/Addition.java
@@ -0,0 +1,17 @@
+package com.basicprogram;
+
+public class Addition {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		      System.out.println("welcome to Java 8 Stream");
+		        System.out.println("hi I am fine");
+		        int a,b,c,d;
+		        a=10;
+		        b=20;
+		        c=a+b;
+		        d=a-b;
+
+	}
+
+}
